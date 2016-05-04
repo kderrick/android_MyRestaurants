3d2a91c67247c59b16363f72c8287ea86189e7a1
Kyle <kderrick1988@gmail.com>
2016-05-04 13:50:56 -0700

    add User model

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
@@ -1,33 +1,33 @@
 package com.epicodus.myrestaurants.ui;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import android.support.v4.view.MenuItemCompat;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.support.v7.widget.SearchView;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
 
-import com.epicodus.myrestaurants.Constants;
-import com.epicodus.myrestaurants.R;
-import com.epicodus.myrestaurants.adapters.RestaurantListAdapter;
-import com.epicodus.myrestaurants.models.Restaurant;
-import com.epicodus.myrestaurants.services.YelpService;
+        import com.epicodus.myrestaurants.Constants;
+        import com.epicodus.myrestaurants.R;
+        import com.epicodus.myrestaurants.adapters.RestaurantListAdapter;
+        import com.epicodus.myrestaurants.models.Restaurant;
+        import com.epicodus.myrestaurants.services.YelpService;
 
-import java.io.IOException;
-import java.util.ArrayList;
+        import java.io.IOException;
+        import java.util.ArrayList;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import okhttp3.Call;
-import okhttp3.Response;
-import okhttp3.Callback;
+        import butterknife.Bind;
+        import butterknife.ButterKnife;
+        import okhttp3.Call;
+        import okhttp3.Response;
+        import okhttp3.Callback;
 
 public class RestaurantListActivity extends AppCompatActivity {
 
