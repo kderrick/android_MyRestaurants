61d0c1e42505786d2bab39bebf268b1594f351b2
Kyle <kderrick1988@gmail.com>
2016-05-04 13:31:55 -0700

    refactor mainactivity and mainactivity layout

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -3,10 +3,12 @@ package com.epicodus.myrestaurants.ui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,64 +20,49 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.HashMap;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
-    private Firebase mSearchedLocationRef;
-    private ValueEventListener mSearchedLocationRefListener;
-
-    @Bind(R.id.locationEditText) EditText mLocationEditText;
-    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton ;
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
-        mSearchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
-
-        mSearchedLocationRefListener = mSearchedLocationRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String locations = dataSnapshot.getValue().toString();
-                Log.d("Location updated", locations);
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
-            }
-        });
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mSearchedLocationRef.removeEventListener(mSearchedLocationRefListener);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        return true;
     }
-            @Override
-            public void onClick(View v) {
-                if (v == mFindRestaurantsButton) {
-                    String location = mLocationEditText.getText().toString();
-                    saveLocationToFirebase(location);
-                    Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-                    intent.putExtra("location", location);
-                    startActivity(intent);
-                }
-                if (v == mSavedRestaurantsButton) {
-                    Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
-                    startActivity(intent);
-                }
-            }
 
-        public void saveLocationToFirebase(String location) {
-            Firebase searchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
-            searchedLocationRef.push().setValue(location);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsButton) {
+            Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
+            startActivity(intent);
         }
+        if (v == mSavedRestaurantsButton) {
+            Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
