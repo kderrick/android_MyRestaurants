0023199809a1c62dee10c3e8a84a8b6dfdf7bc44
Kyle <kderrick1988@gmail.com>
2016-05-05 11:33:15 -0700

    add auto populate email from login form via shared preferences

--- app/src/main/java/com/epicodus/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/LoginActivity.java
@@ -47,6 +47,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mAuthProgressDialog.setTitle("Loading...");
         mAuthProgressDialog.setMessage("Authenticating with Firebase...");
         mAuthProgressDialog.setCancelable(false);
+        String signupEmail = mSharedPreferences.getString(Constants.KEY_USER_EMAIL, null);
+        if (signupEmail != null) {
+            mEmailEditText.setText(signupEmail);
+        }
     }
 
     @Override
@@ -62,7 +66,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     public void loginWithPassword() {
-        String email = mEmailEditText.getText().toString();
+        final String email = mEmailEditText.getText().toString();
         String password = mPasswordEditText.getText().toString();
 
         if (email.equals("")) {
@@ -77,6 +81,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler() {
             @Override
             public void onAuthenticated(AuthData authData) {
+                mSharedPreferencesEditor.putString(Constants.KEY_USER_EMAIL, email).apply();
                 mAuthProgressDialog.dismiss();
                 if (authData != null) {
                     String userUid = authData.getUid();
